ae144d4fffe3460d4d5513a61d88aa8b6f7677e6
Zezao132 <124840502+Zezao132@users.noreply.github.com>
2024-02-22 22:22:14 -0300

    Add files via upload

---
+++ atividade José Matheus.js
@@ -0,0 +1,70 @@
+import React, { useState } from 'react';
+import { View, Text, TextInput, Button, Image, StyleSheet } from 'react-native';
+
+const LoginScreen = () => {
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+
+  const handleLogin = () => {
+    // Aqui você pode adicionar a lógica de autenticação
+    console.log('Email:', email);
+    console.log('Senha:', password);
+    // Por exemplo, você pode enviar os dados para um servidor para autenticação
+  };
+
+  return (
+    <View style={styles.container}>
+      <Image
+        source={require('./transferir.png')}
+        style={styles.logo}
+      />
+      <Text style={styles.title}>Login</Text>
+      <TextInput
+        style={styles.input}
+        placeholder="Email"
+        onChangeText={setEmail}
+        value={email}
+        keyboardType="email-address"
+        autoCapitalize="none"
+      />
+      <TextInput
+        style={styles.input}
+        placeholder="Senha"
+        onChangeText={setPassword}
+        value={password}
+        secureTextEntry
+      />
+      <Button title="Login" onPress={handleLogin} />
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    paddingHorizontal: 20,
+  },
+  logo: {
+    width: 200,
+    height:50,
+    marginBottom: 20,
+  },
+  title: {
+    fontSize: 24,
+    fontWeight: 'bold',
+    marginBottom: 20,
+  },
+  input: {
+    width: '100%',
+    height: 40,
+    borderWidth: 1,
+    borderColor: '#ccc',
+    borderRadius: 5,
+    paddingHorizontal: 10,
+    marginBottom: 10,
+  },
+});
+
+export default LoginScreen;
